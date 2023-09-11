ff5a89da6814c46820c4816883c03a7b76244596
mohammedmoin2001 <124486931+mohammedmoin2001@users.noreply.github.com>
2023-09-11 09:41:31 +0530

    ikage recognition

---
+++ image-recognition-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ImageRecognition from './ImageRecognition';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ImageRecognition />
+      </header>
+    </div>
+  );
+}
+
+export default App;
